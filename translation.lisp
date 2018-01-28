ab960ceb6d4f1ecb90166314aadd012de7a216ba
Foobar <4829djaskdfj@gmail.com>
2018-01-27 21:09:07 -0800

    WIP for google translate api instead of transltr

--- translation.lisp
+++ translation.lisp
@@ -3,34 +3,29 @@
 
 (defgeneric translate (client text))
 
+;;; Source: https://gist.github.com/longfanos/87fcf04579214424d2fe
+;; var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
+;; + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
 
-(define-constant +transltr-api-address+ "http://www.transltr.org/api/"
-  :test #'string=)
+(defparameter *source-language* "ja")
+(defparameter *target-language* "en")
 
+(defparameter *google-translate-api-address*
+  (format nil "https://translation.googleapis.com/translate_a/single?client=gtx&sl=~A&tl=~A&dt=t&q="
+          *source-language* *target-language*))
 
-(define-constant +user-agent+ "grateful-transltr-client"
-  :test #'string=)
+(defclass google-translate-client ()
+  ((translate-addr :initform (format nil "~A"
+                                     *google-translate-api-address*))))
 
-
-(defclass transltr-client ()
-  ((translate-addr :initform (format nil "~A~A"
-                                       +transltr-api-address+
-                                       "translate"))))
-
-
-(defmethod translate ((this transltr-client) (text string))
+(defmethod translate ((this google-translate-client) (text string))
   (with-slots (translate-addr headers) this
     (handler-case
-        (let* ((content (jsown:to-json (jsown:new-js ("text" text)
-                                                     ("to" "en")
-                                                     ("from" "ja"))))
-               (answer (drakma:http-request translate-addr
-                                            :method :post
-                                            :content content
-                                            :content-type "application/json"
-                                            :user-agent +user-agent+
-                                            :external-format-out :utf-8
-                                            :force-binary t))
-               (json (octets-to-string answer :external-format :utf-8)))
-          (jsown:val (jsown:parse json) "translationText"))
+        (let* ((answer (drakma:http-request (concatenate 'string translate-addr (drakma:url-encode text :utf-8))
+                                            :method :get
+                                            :content-type "application/json" ;; "text/html"
+                                            :external-format-out :utf-8)))
+          ;; TODO
+          (jsown:val (jsown:parse answer ;; json
+                                  ) "data"))
       (t (e) (log:error "Error during translation request: ~A" e) nil))))
